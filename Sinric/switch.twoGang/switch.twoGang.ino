3d6350fe67a075e6ac9eb5b18aae898f607b1013
Aaron Dutenhoefer <aarner@gmail.com>
2018-12-30 18:14:18 -0600

    added sinric-based two gang switch

--- Sinric/switch.twoGang/switch.twoGang.ino
+++ Sinric/switch.twoGang/switch.twoGang.ino
@@ -17,28 +17,37 @@ WiFiClient client;
 #define MyApiKey "d66b0116-ac4f-43ed-9087-5d0e154554c4" 
 #define MySSID "Dutenhoefer"        
 #define MyWifiPassword "CepiCire99" 
-#define DEVICE_ID "5c24624f081d70507895b2e6"  // deviceId is the ID assgined to your smart-home-device in sinric.com dashboard. Copy it from dashboard and paste it here
+#define DEVICE_ID1 "5c27cd56c37e0372a25b6fdd"  // deviceId is the ID assgined to your smart-home-device in sinric.com dashboard. Copy it from dashboard and paste it here
+#define DEVICE_ID2 "5c27cd6ac37e0372a25b6fde"  // deviceId is the ID assgined to your smart-home-device in sinric.com dashboard. Copy it from dashboard and paste it here
+
 #define HEARTBEAT_INTERVAL 300000 // 5 Minutes 
-#define LAN_HOSTNAME  "OtaTestTwo"
+#define LAN_HOSTNAME  "TangoGolf1-2"
 /************ define pins *********/
-#define RELAY   D1   // D1 drives 120v relay
-#define CONTACT D3   // D3 connects to momentary switch
-#define LED D4       // D4 powers switch illumination
+#define RELAY_ONE D7     // D7 drives 120v relay
+#define RELAY_TWO D0     // D0 drives 120v relay
+#define CONTACT_ONE D2   // D2 connects to momentary switch
+#define CONTACT_TWO D3   // D3 connects to momentary switch
+#define LED_ONE D5       // D5 powers switch illumination
+#define LED_TWO D6       // D6 powers switch illumination
 
 uint64_t heartbeatTimestamp = 0;
-bool isConnected = false;
-boolean lightState = false;
+boolean isConnected = false;
+boolean lightStateOne = false;
+boolean lightStateTwo = false;
 
 void setPowerStateOnServer(String deviceId, String value);
-void lightsOn(boolean notifyServer);  
-void lightsOff(boolean notifyServer);
+void lightsOn(uint8_t relayPin, uint8_t ledPin, String deviceId, boolean notifyServer);  
+void lightsOff(uint8_t relayPin, uint8_t ledPin, String deviceId, boolean notifyServer);
 
 void turnOn(String deviceId) {
-  if (deviceId == DEVICE_ID) // Device ID of first device
-  {  
+  if (deviceId == DEVICE_ID1) { // Device ID of first device 
     Serial.print("Turn on device id: ");
     Serial.println(deviceId);
-    lightsOn(false);
+    lightsOn(RELAY_ONE, LED_ONE, deviceId, false);
+  } else if (deviceId == DEVICE_ID2) {
+    Serial.print("Turn on device id: ");
+    Serial.println(deviceId);
+    lightsOn(RELAY_TWO, LED_TWO, deviceId, false);  
   } else {
     Serial.print("Turn on for unknown device id: ");
     Serial.println(deviceId);    
@@ -46,11 +55,14 @@ void turnOn(String deviceId) {
 }
 
 void turnOff(String deviceId) {
-   if (deviceId == DEVICE_ID) // Device ID of first device
-   {  
+   if (deviceId == DEVICE_ID1) { // Device ID of first device
      Serial.print("Turn off Device ID: ");
      Serial.println(deviceId);
-     lightsOff(false);
+     lightsOff(RELAY_ONE, LED_ONE, deviceId, false);
+   } else if (deviceId == DEVICE_ID2) {
+     Serial.print("Turn off Device ID: ");
+     Serial.println(deviceId);
+     lightsOff(RELAY_TWO, LED_TWO, deviceId, false);
    } else {
      Serial.print("Turn off for unknown device id: ");
      Serial.println(deviceId);    
@@ -105,13 +117,18 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
 
 
 void setup() {
+  pinMode(LED_ONE, OUTPUT);
+  pinMode(RELAY_ONE, OUTPUT);
+  pinMode(CONTACT_ONE, INPUT_PULLUP);
 
-  pinMode(LED, OUTPUT);
-  pinMode(RELAY, OUTPUT);
-  pinMode(CONTACT, INPUT_PULLUP);
+  pinMode(LED_TWO, OUTPUT);
+  pinMode(RELAY_TWO, OUTPUT);
+  pinMode(CONTACT_TWO, INPUT_PULLUP);
   
-  digitalWrite(LED, HIGH);   // default switch illumination to on
-  digitalWrite(RELAY, LOW);  // shut off the high voltage relay side
+  digitalWrite(LED_ONE, HIGH);   // default switch illumination to on
+  digitalWrite(LED_TWO, HIGH);   // default switch illumination to on
+  digitalWrite(RELAY_ONE, HIGH);  // shut off the high voltage relay side
+  digitalWrite(RELAY_TWO, HIGH);  // shut off the high voltage relay side
   
   Serial.begin(115200);
   
@@ -122,7 +139,7 @@ void setup() {
 
   // Waiting for Wifi connect
   while(WiFiMulti.run() != WL_CONNECTED) {
-    delay(1000);
+    delay(250);
     Serial.print(".");
   }
   if(WiFiMulti.run() == WL_CONNECTED) {
@@ -174,32 +191,49 @@ void loop() {
           webSocket.sendTXT("H");          
       }
   }   
-    boolean buttonState = digitalRead(CONTACT);
-    if (buttonState == LOW) {
-      if(lightState == true) {
-        lightsOff(true);
+    boolean buttonStateOne = digitalRead(CONTACT_ONE);
+    if (buttonStateOne == LOW) {
+      if(lightStateOne == true) {
+        lightsOff(RELAY_ONE, LED_ONE, DEVICE_ID1, true);
       } else {
-        lightsOn(true);
+         lightsOn(RELAY_ONE, LED_ONE, DEVICE_ID1, true);
+      }
+      //affect a debounce - keep momentary switch looping multiple times/sec
+      delay(333);
+    }
+
+    boolean buttonStateTwo = digitalRead(CONTACT_TWO);
+    if (buttonStateTwo == LOW) {
+      if(lightStateTwo == true) {
+        lightsOff(RELAY_TWO, LED_TWO, DEVICE_ID2, true);
+      } else {
+         lightsOn(RELAY_TWO, LED_TWO, DEVICE_ID2, true);
       }
       //affect a debounce - keep momentary switch looping multiple times/sec
       delay(333);
     }
 }
 
-void lightsOn(boolean notifyServer) {
-    Serial.println("Switch 1 turn on ...");
-    digitalWrite(LED, LOW);
-    digitalWrite(RELAY, HIGH);
-    lightState = true;
-    if (notifyServer) { setPowerStateOnServer(DEVICE_ID, "ON"); }
+void lightsOn(uint8_t relayPin, uint8_t ledPin, String deviceId, boolean notifyServer) {
+    Serial.print("Switch ");
+    Serial.print(deviceId);
+    Serial.println(" turn on ...");
+    digitalWrite(ledPin, LOW);
+    digitalWrite(relayPin, LOW);
+    if (deviceId == DEVICE_ID1) { lightStateOne=true; } 
+    if (deviceId == DEVICE_ID2) { lightStateTwo=true; }
+    if (notifyServer) { setPowerStateOnServer(deviceId, "ON"); }
 }
 
-void lightsOff(boolean notifyServer) {
-    Serial.println("Switch 1 turn off ...");
-    digitalWrite(LED, HIGH);
-    digitalWrite(RELAY, LOW);
-    lightState = false;
-    if (notifyServer) { setPowerStateOnServer(DEVICE_ID, "OFF"); }
+void lightsOff(uint8_t relayPin, uint8_t ledPin, String deviceId, boolean notifyServer) {
+    Serial.print("Switch ");
+    Serial.print(deviceId);
+    Serial.println(" turn off ...");
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(relayPin, HIGH);
+    if (deviceId == DEVICE_ID1) { lightStateOne=false; } 
+    if (deviceId == DEVICE_ID2) { lightStateTwo=false; }
+    if (notifyServer) { setPowerStateOnServer(deviceId, "OFF"); }
 }
 
 // If you are going to use a push button to on/off the switch manually, use this function to update the status on the server
